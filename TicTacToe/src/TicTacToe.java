e55620ec5c05bc214af07eb7c5dd0416f9ecad13
Taha-Sid <74993189+Taha-Sid@users.noreply.github.com>
2024-09-01 14:19:14 +0100

    Add files via upload
    added variables for turn and JFrame and JPanel for frame, buttons and textfields

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -4,6 +4,15 @@ import java.util.*;
 import javax.swing.*;
 
 public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn; //true if p1 turn, false if p2 turn, so no need for p2 variable
+
     TicTacToe() {}
 
     @Override
