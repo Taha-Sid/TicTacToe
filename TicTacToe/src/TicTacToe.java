19494b63507631077fda6d2cf761fefa6498dada
Taha-Sid <74993189+Taha-Sid@users.noreply.github.com>
2024-09-01 15:03:39 +0100

    Add files via upload
    did the same for player 2

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -137,8 +137,52 @@ public class TicTacToe implements ActionListener {
         (buttons[6].getText()=="X")) {
             xWins(2,4,6);
         }
-
         //check O win conditions
+        if((buttons[0].getText()=="O") &&
+                (buttons[1].getText()=="O") &&
+                (buttons[2].getText()=="O")) {
+            oWins(0,1,2);
+        }
+        if((buttons[3].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[5].getText()=="O")) {
+            oWins(3,4,5);
+        }
+        if((buttons[6].getText()=="O") &&
+                (buttons[7].getText()=="O") &&
+                (buttons[8].getText()=="O")) {
+            oWins(6,7,8);
+        }
+        if((buttons[0].getText()=="O") &&
+                (buttons[3].getText()=="O") &&
+                (buttons[6].getText()=="O")) {
+            oWins(0,3,6);
+        }
+        if((buttons[0].getText()=="O") &&
+                (buttons[1].getText()=="O") &&
+                (buttons[2].getText()=="O")) {
+            oWins(0,1,2);
+        }
+        if((buttons[1].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[7].getText()=="O")) {
+            oWins(1,4,7);
+        }
+        if((buttons[2].getText()=="O") &&
+                (buttons[5].getText()=="O") &&
+                (buttons[8].getText()=="O")) {
+            oWins(2,5,8);
+        }
+        if((buttons[0].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[8].getText()=="O")) {
+            oWins(0,4,8);
+        }
+        if((buttons[2].getText()=="O") &&
+                (buttons[4].getText()=="O") &&
+                (buttons[6].getText()=="O")) {
+            oWins(2,4,6);
+        }
     }
 
     public void xWins(int a, int b, int c) {}
